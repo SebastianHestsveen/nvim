e703c0ae9239bbbe307c1503dfeb9e282ff72cda
Sebastian Hestsveen <sebashes@stud.ntnu.no>
2024-06-09 19:31:35 +0200

    add new keybindes

--- lua/sh/keybinds.lua
+++ lua/sh/keybinds.lua
@@ -1,2 +1,8 @@
-vim.g.mapleader = " " 
+vim.g.mapleader = " "
+
 vim.keymap.set("n", "<leader>ex", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>w", vim.cmd.w)
+vim.keymap.set("n", "<leader>q", vim.cmd.q)
+vim.keymap.set("n", "<leader>so", vim.cmd.so)
+--it lets me replace the word under my cursor in the complete file 
+vim.keymap.set("n", "<leader>r", ":%s/<C-r><C-w>//g<Left><Left>")
