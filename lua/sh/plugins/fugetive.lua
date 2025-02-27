f410f3a017c56f34877b45639a1898b09c75510f
SebastianHestsveen <sebba00@gmail.com>
2025-02-27 21:37:39 +0100

    test

--- lua/sh/plugins/fugetive.lua
+++ lua/sh/plugins/fugetive.lua
@@ -17,7 +17,7 @@ return {
         vim.keymap.set("n", "<leader>gc", function()
             local commit_msg = vim.fn.input("Commit message: ")
             if commit_msg ~= "" then
-                vim.cmd.Git({ "commit", "-m", commit_msg })
+                vim.cmd("Git commit -m " .. vim.fn.shellescape(commit_msg))
             else
                 print("Aborted: No commit message provided.")
             end
