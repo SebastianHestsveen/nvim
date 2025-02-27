374a22da0e8fa7051fa4f2440917b5e591dad5f1
SebastianHestsveen <sebba00@gmail.com>
2025-02-27 21:19:27 +0100

    fugetiv adde

---
+++ lua/sh/plugins/fugetive.lua
@@ -0,0 +1,15 @@
+return {
+    "tpope/vim-fugitive",
+
+    lazy = false,
+
+    config = function()
+        vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+        vim.keymap.set("n", "<leader>gP", function()
+            vim.cmd.Git({ "pull" })
+        end)
+        vim.keymap.set("n", "<leader>gp", function()
+            vim.cmd.Git({ "push" })
+        end)
+    end,
+}
